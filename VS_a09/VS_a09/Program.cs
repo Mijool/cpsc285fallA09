09f9fa31fad24f4e1972e0d2111dcc2d2fc1df34
Camarena, Miguel <mcamarena@my.dom.edu>
2025-11-26 12:50:29 -0600

    NewSkeleton-Usable

--- VS_a09/VS_a09/Program.cs
+++ VS_a09/VS_a09/Program.cs
@@ -16,7 +16,7 @@ namespace VS_a09
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmDrivingSchool());
         }
     }
 }
--- VS_a09/VS_a09/Properties/Settings.Designer.cs
+++ VS_a09/VS_a09/Properties/Settings.Designer.cs
@@ -8,22 +8,28 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace VS_a09.Properties
-{
-
-
+namespace VS_a09.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.7.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=cissql;Initial Catalog=F25_285A;Integrated Security=True")]
+        public string F25_285AConnectionString {
+            get {
+                return ((string)(this["F25_285AConnectionString"]));
             }
         }
     }
